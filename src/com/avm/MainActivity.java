566438a3c52c031ec0e5e6f77d1d39eeedaa3457
Antonio Vicente Martin <antoniovicente.avm@gmail.com>
2013-12-03 18:29:27 +0100

    Make minor changes

--- src/com/avm/MainActivity.java
+++ src/com/avm/MainActivity.java
@@ -1,15 +1,62 @@
 package com.avm;
 
-import android.os.Bundle;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.SocketException;
+
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+	
+	private Socket s;
+	private DatagramSocket datagramSocket;
+	private DatagramPacket datagramPacket;
+	
+	private byte [] data;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		data = new byte [100];
+		
+		TextView tv = (TextView)findViewById(R.id.hello);
+		
+		s = new Socket();
+			
+
+		
+		try {
+			s.connect(new InetSocketAddress("192.168.3.132", 3333));
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+		
+		
+		try {
+			datagramSocket = new DatagramSocket(0);
+		} catch (SocketException e) {
+			e.printStackTrace();
+		}
+		
+		datagramPacket = new DatagramPacket(data, data.length);
+		
+		try {
+			datagramSocket.receive(datagramPacket);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		tv.setText(new String(datagramPacket.getData()));
+		
+		
 	}
 
 	@Override
