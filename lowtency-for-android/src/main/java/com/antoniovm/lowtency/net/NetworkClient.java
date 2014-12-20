9f553c99f3e9d01b74c2f18907ebd4ee96328d90
Antonio Vicente Martin <antoniovicente.avm@gmail.com>
2014-12-20 22:48:33 +0100

    Update submodule byte-utils-for-java

--- lowtency-for-android/src/main/java/com/antoniovm/lowtency/net/NetworkClient.java
+++ lowtency-for-android/src/main/java/com/antoniovm/lowtency/net/NetworkClient.java
@@ -1,5 +1,7 @@
 package com.antoniovm.lowtency.net;
 
+import com.antoniovm.lowtency.core.StreamHeader;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.DatagramPacket;
@@ -7,89 +9,85 @@ import java.net.DatagramSocket;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 
-import com.antoniovm.lowtency.core.StreamHeader;
-
 /**
  * @author Antonio Vicente Martin
- * 
  */
 public class NetworkClient {
 
-	private Socket socket;
-	private DatagramSocket datagramSocket;
-	private DatagramPacket datagramPacket;
-	private StreamHeader streamHeader;
+    private Socket socket;
+    private DatagramSocket datagramSocket;
+    private DatagramPacket datagramPacket;
+    private StreamHeader streamHeader;
 
-	/**
-	 * 
-	 */
-	public NetworkClient(int bufferLength) {
-		this.datagramPacket = new DatagramPacket(new byte[bufferLength], bufferLength);
-		this.streamHeader = new StreamHeader();
-		this.socket = new Socket();
-	}
+    /**
+     *
+     */
+    public NetworkClient(int bufferLength) {
+        this.datagramPacket = new DatagramPacket(new byte[bufferLength], bufferLength);
+        this.streamHeader = new StreamHeader();
+        this.socket = new Socket();
+    }
 
-	/**
-	 * Receives a datagram socket from
-	 */
-	public byte[] receiveUDP() {
-		try {
-			datagramSocket.receive(datagramPacket);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+    /**
+     * Receives a datagram socket from
+     */
+    public byte[] receiveUDP() {
+        try {
+            datagramSocket.receive(datagramPacket);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-		return datagramPacket.getData();
-	}
+        return datagramPacket.getData();
+    }
 
-	/**
-	 * Receives and returns the header information about the stream that is
-	 * going to be received
-	 * 
-	 * @return streamHeader The header information of the stream
-	 */
-	public StreamHeader receiveHeader() {
-		byte[] serializedHeader = new byte[4];
+    /**
+     * Receives and returns the header information about the stream that is
+     * going to be received
+     *
+     * @return streamHeader The header information of the stream
+     */
+    public StreamHeader receiveHeader() {
+        byte[] serializedHeader = new byte[4];
 
-		try {
-			InputStream is = socket.getInputStream();
-			is.read(serializedHeader);
-			is.close();
-			StreamHeader.buildFromSerialized(serializedHeader, streamHeader);
+        try {
+            InputStream is = socket.getInputStream();
+            is.read(serializedHeader);
+            is.close();
+            StreamHeader.buildFromSerialized(serializedHeader, streamHeader);
 
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-		//int bufferSize = streamHeader.getBufferSize();
-		//this.datagramPacket = new DatagramPacket(new byte[bufferSize], bufferSize);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-		return streamHeader;
+        //int bufferSize = streamHeader.getBufferSize();
+        //this.datagramPacket = new DatagramPacket(new byte[bufferSize], bufferSize);
 
-	}
+        return streamHeader;
 
-	/**
-	 * 
-	 * Makes a TCP conecction
-	 * 
-	 * @param ip
-	 * @param port
-	 */
-	public boolean connect(String host, int port) {
-		if (socket.isConnected()) {
-			return false;
-		}
+    }
 
-		try {
-			this.socket.connect(new InetSocketAddress(host, port));
-			this.datagramSocket = new DatagramSocket(socket.getLocalPort());
-			this.datagramSocket.setReuseAddress(true);
-		} catch (IOException e) {
-			e.printStackTrace();
-			return false;
-		}
+    /**
+     * Makes a TCP conecction
+     *
+     * @param ip
+     * @param port
+     */
+    public boolean connect(String host, int port) {
+        if (socket.isConnected()) {
+            return false;
+        }
 
-		return true;
-	}
+        try {
+            this.socket.connect(new InetSocketAddress(host, port));
+            this.datagramSocket = new DatagramSocket(socket.getLocalPort());
+            this.datagramSocket.setReuseAddress(true);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+
+        return true;
+    }
 
 }
